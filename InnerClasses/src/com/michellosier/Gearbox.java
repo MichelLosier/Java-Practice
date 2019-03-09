e55703d56d9e911af1472ea0508938d5f049d2d5
MichelLosier <mikelosier@gmail.com>
2019-03-09 15:24:33 -0800

    inner classes and spring

---
+++ InnerClasses/src/com/michellosier/Gearbox.java
@@ -0,0 +1,69 @@
+package com.michellosier;
+
+import java.util.ArrayList;
+
+public class Gearbox {
+
+    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int currentGear = 0;
+    private boolean clutchIsIn;
+
+    public Gearbox(int maxGears) {
+        this.gears = new ArrayList<Gear>();
+        this.maxGears = maxGears;
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
+    }
+
+    public void operateClutch(boolean in){
+        this.clutchIsIn = in;
+    }
+
+    public void addGear(int number, double ratio){
+        if((number > 0) && (number <= maxGears)){
+            this.gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int newGear){
+        if((newGear >= 0) && (newGear < this.gears.size() && this.clutchIsIn)){
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected.");
+        } else {
+            System.out.println("Grind!");
+            this.currentGear = 0;
+        }
+    }
+
+    public double wheelSpeed(int revs){
+        if(clutchIsIn){
+            System.out.println("Scream!");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
+    }
+
+    //this is useful if it never makes sense to refer to a Gear outside of context
+    //of a GearBox - cases where coupling is desired,
+    // and decoupling just adds to complexity
+    private class Gear {
+        private int gearNumber;
+        private double ratio;
+
+        public Gear(int gearNumber, double ratio) {
+            this.gearNumber = gearNumber;
+            this.ratio = ratio;
+        }
+
+        public double getRatio() {
+            return ratio;
+        }
+
+        public double driveSpeed(int revs) {
+            return revs * (this.ratio);
+        }
+    }
+
+
+}
---
+++ InnerClasses/src/com/michellosier/Main.java
@@ -0,0 +1,11 @@
+package com.michellosier;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Gearbox mcLaren = new Gearbox(6);
+
+		mcLaren.addGear(1, 12.3);
+
+    }
+}
